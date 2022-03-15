8daa9cdf9b83dd500a6af2f4c11f0eedd814daf6
kimtaeil <xodlf5859@naver.com>
2022-03-15 10:34:06 +0900

    0315 연습

--- src/main/java/com/az/board/controller/BoardController.java
+++ src/main/java/com/az/board/controller/BoardController.java
@@ -1,12 +1,12 @@
 package com.az.board.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.az.board.service.BoardService;
@@ -19,18 +19,17 @@ public class BoardController {
 	@Autowired
 	BoardService boardService;
 	
-	
-	@RequestMapping("/select")
-	public ModelAndView boardSelect(ModelAndView mnv,BoardVO boardVO) throws Exception{
+	@GetMapping("/select")
+	@ResponseBody
+	public ModelAndView selectBoard(BoardVO boardVO,ModelAndView mnv) throws Exception{
+		List<BoardVO> selectList = boardService.selectBoard(boardVO);
 		
-		Map<String, Object> dataMap = new HashMap<String, Object>();
-		List<BoardVO> boardList = boardService.selectBoard(boardVO);
+		mnv.addObject("selectList", selectList);
+		mnv.setViewName("board/select");
 		
-		dataMap.put("boardList", boardList);
-		
-		mnv.setViewName("board/boardTest");
-		mnv.addObject("dataMap",dataMap);
-		
+		System.out.println(selectList.get(0).getBoardIdx());
 		return mnv;
 	}
+	
+	
 }
